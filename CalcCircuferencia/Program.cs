533c21e050f7dd0a96218bb0d1515863d5d0ab77
henriquelandim <32872996+henriquelandim@users.noreply.github.com>
2018-04-21 21:27:32 -0300

    Exercício de logíca, Calculadora de Circuferência
    não esta comentado detalhadamente, mas esta bem simples de entender.

---
+++ CalcCircuferencia/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Objetivo criar uma calculadora que calcula a area, a circunferencia e o diametro de um circulo
+tendo como informação o raio, para o diametro */
+
+
+using System; 
+
+namespace CalcCircuferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   double pi = 3.14/*15926535897932384626433832795028841971*/; Console.WriteLine ("Especifique o raio do circulo:");
+			double raio = double.Parse (Console.ReadLine ());
+            Console.WriteLine ("Diâmetro = " +(raio+raio));
+			Console.WriteLine ("Circunferência = "+(2 * pi * raio)); //.ToString());
+			Console.WriteLine ("Área = "+(pi * (raio * raio)).ToString());
+        }
+    }
+}
